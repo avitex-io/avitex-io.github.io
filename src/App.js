a4bba7ffe37360bf79426755b52550ec9f3a88ec
joshua-small <small.joshua@gmail.com>
2023-10-19 02:34:21 -0700

    Integrated Semantic UI React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+import React from 'react';
+import { Button } from 'semantic-ui-react';
 
 function App() {
   return (
